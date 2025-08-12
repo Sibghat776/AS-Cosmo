8e5192bb8951b907a47826497d2f4a869c1187aa
Sibghat776 <ullahsibghat786@gmail.com>
2025-08-12 21:07:45 +0500

    notupdated

--- client side/src/components/productCard/ProductCard.jsx
+++ client side/src/components/productCard/ProductCard.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 
 const ProductCard = ({ product }) => {
   return (
-<Link to={`/product/${product._id}`}>
-    <div className="productCard">
-      <img src={product?.images[0].url} alt="" />
-      {
-        product?.name?.length < 15 ?
-        <h2 style={{textAlign: "center"}}>{product.name}</h2> :
-        <h2 style={{textAlign: "center"}}>{product.name.slice(0, 15)}....</h2> 
-      }
-      <p style={{textAlign: "center"}}>{product.description.slice(0, 50)}....</p>
-      <p>Rs.{product?.price}/-</p>
-    </div>
+    <Link to={`/product/${product._id}`}>
+      <div className="productCard">
+        <img src={product?.images[0].url} alt="" />
+        {
+          product?.name?.length < 15 ?
+            <h2 style={{ textAlign: "center" }}>{product.name}</h2> :
+            <h2 style={{ textAlign: "center" }}>{product.name.slice(0, 15)}....</h2>
+        }
+        <p style={{ textAlign: "center" }}>{product.description.slice(0, 50)}....</p>
+        <p>Rs.{product?.price}/-</p>
+      </div>
     </Link>
   );
 };
